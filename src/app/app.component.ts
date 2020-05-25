ac18e2c81b3a33b87dbb93e2d564137a3d643835
Kevin Hernandez <kevhdez@Kevins-MacBook-Pro.local>
2020-05-24 21:01:42 -0400

    Subida de proyecto hecho en  ionic 5 con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,41 +9,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
+
 export class AppComponent implements OnInit {
-  public selectedIndex = 0;
+  public selectedIndexAppPage = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Timer',
+      url: '/folder/Timer',
+      icon: 'timer'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'What Pomodoro really is',
+      url: '/maintenance/PomoMeaning',
+      icon: ''
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      title: 'Settings',
+      url: '/maintenance/settings',
+      icon: ''
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
@@ -63,7 +48,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const path = window.location.pathname.split('folder/')[1];
     if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+      this.selectedIndexAppPage = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
     }
   }
 }
